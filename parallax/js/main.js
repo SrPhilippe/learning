55234a46e51b1ab54abe45330932f127988a7c36
SrPhilippe <philippehenrique@outlook.com>
2018-06-30 11:10:26 -0300

    updating and fixes

--- parallax/js/main.js
+++ parallax/js/main.js
@@ -1,14 +1,14 @@
 $(document).ready(() => {
 
-	$(window).scroll(() => {
-		var topScroll = $(this).scrollTop();
-		
-		$(".parallax").css({
-			"transition": "none",
-			"backgroundPosition": "center " + (topScroll * 0.5) + "px"
-		});
+    $(window).scroll(() => {
+        var topScroll = $(this).scrollTop()
 
-		console.log($(".parallax").css("background-position"));
-	});
+        $(".parallax").css({
+            transition: "none",
+            backgroundPosition: `0% ${- topScroll * 0.5 + "px"}` // If the parallax will occur on the top just let the value of (y) position positive
+        })
 
-});
+        console.log($(".parallax").css("background-position"))
+    })
+
+})
