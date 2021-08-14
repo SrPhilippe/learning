abc961815f1c4ebd4b2138760931f25692c9f9b0
SrPhilippe <philippehenrique@outlook.com>
2021-08-13 22:51:23 -0300

    update

---
+++ javascript/index.js
@@ -0,0 +1,3 @@
+for (let x = 0; x < 5; x++)
+  for (let y = 0; y < 3; y++)
+    console.log(`${x}, ${y}`)
---
+++ javascript/part3/index4-ternaryOperator.js
@@ -0,0 +1,19 @@
+/* 
+
+let isGoing = true;
+let color;
+
+if (isGoing === true) {
+    color = "Green";
+} else {
+    color = "Red";
+}
+
+ */
+
+// * This is a shorthand for the code above
+
+let isGoing = true,
+    color = isGoing ? "Green" : "Red"
+
+console.log(color)
