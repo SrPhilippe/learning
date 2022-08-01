e0e8d965fb6d8145cd76bb31c4b44adab1bd4346
Philippe <philippehenrique@outlook.com>
2022-08-01 19:03:22 -0300

    big update 1.3

--- aClin/autorize.js
+++ aClin/autorize.js
@@ -9,4 +9,13 @@ if (aclinpath == "/CadInventario.php5") {
 	// $button.forEach(el => {
 	// 	el.removeAttribute("disabled")
 	// })
+} else if (aclinpath == "/OSBiomedicaEmergencia.php5") {
+	let $buttonPDF = document.createElement("a"),
+		$tudo = document.querySelector('#tudo'),
+		numberOS = "23135"
+	
+	$buttonPDF.classList.add("mm-button", "mm-theme-l1")
+	$buttonPDF.innerHTML = "PDF"
+	$buttonPDF.setAttribute('href', `https://aclin-check.com.br/Relatorio_OSMC.php?CODE=${numberOS}&CNPJ=17178203000680&Caminho_Imagem=/public_html/ImageLogo/17178203000680.jpg`)
+	$tudo.children.item(1).append($buttonPDF)
 }
--- aClin/main.js
+++ aClin/main.js
@@ -4,37 +4,40 @@ let locate = new URL(location.href).searchParams.get('code')
  */
 
 const myRequest = 'https://raw.githubusercontent.com/SrPhilippe/learning/master/aClin/style.css',
-      url = document.location
+  url = document.location,
+  version = "1.3"
+
+
+// This prevent some bugs when you open the aClin initial page outside the system
+if (url.pathname != "/index.html") {
+  addStyle(myRequest)
+  console.log(`Css custom style applied!`)
+}
 
 function addStyle(source) {
   const $body = document.getElementsByTagName("body")[0],
     $style = document.createElement("style")
-    $style.classList.add('aClin-custom-css')
 
+  $style.classList.add('aClin-custom-css')
   $style.setAttribute("type", "text/css")
 
   fetch(source)
     .then(res => res.text())
-    .then (text => {
-        $style.innerHTML = text
-        $body.prepend($style)
+    .then(text => {
+      $style.innerHTML = text
+      $body.prepend($style)
     })
     .catch(err => console.log(err))
 }
 
-if (url.pathname != "/index.html") {
-  addStyle(myRequest)
-}
-
-
-let	elMiddle = document.querySelector('#centro'),
-    elLeft = document.querySelector('#esquerda'),
-    elRight = document.querySelector('#direita'),
-    mmThemel5 = document.querySelector('body > .mm-container.mm-theme-l5'),
-    mainMenu = document.querySelector('.mm-bar.mm-blue.mm-left-align.mm-large'),
-    mainSelection = mmThemel5.firstElementChild,
-    definedSelection,
-    hasParent = false
+let elMiddle = document.querySelector('#centro'),
+  elLeft = document.querySelector('#esquerda'),
+  elRight = document.querySelector('#direita'),
+  mmThemel5 = document.querySelector('body > .mm-container.mm-theme-l5'),
+  mainMenu = document.querySelector('.mm-bar.mm-blue.mm-left-align.mm-large'),
+  mainSelection = mmThemel5.firstElementChild,
+  definedSelection,
+  hasParent = false
 
 if (mainSelection.children.length == 2) {
   hasParent = true
@@ -43,13 +46,13 @@ if (mainSelection.children.length == 2) {
 } else {
   definedSelection = mainSelection
 }
-    
-  let mainContainers = Array.from(definedSelection.children)
 
- mainContainers.forEach(el => {
-   if (el.hasAttribute('id')) {
-     el.removeAttribute('class')
-   }
+let mainContainers = Array.from(definedSelection.children)
+
+mainContainers.forEach(el => {
+  if (el.hasAttribute('id')) {
+    el.removeAttribute('class')
+  }
 })
 
 elMiddle.classList.add('col-lg-8');
@@ -60,11 +63,11 @@ elRight.classList.add('col-lg-2');
 if (hasParent) {
   const container = document.createElement('div')
   container.classList.add('.container')
-  
+
   // Centralize the title
   mmThemel5.querySelector('.mm-row').firstElementChild.classList.replace('text-left', 'text-center')
-  
-  
+
+
   elMiddle.children.item(1).firstElementChild.classList.add('col-lg-6')
   elMiddle.children.item(5).classList.add('col-lg-6')
   elMiddle.children.item(1).appendChild(elMiddle.children.item(5))
@@ -75,15 +78,20 @@ if (hasParent) {
 
 // This part of the code is going to create an UL element inside the menu
 let ulElement = document.createElement('ul'),
-    menuItems = Array.from(mainMenu.children)
+  menuItems = Array.from(mainMenu.children)
 
 menuItems.forEach(el => {
-	if(el.tagName !== "P") {
-      ulElement.append(el)
-    }
+  if (el.tagName !== "P") {
+    ulElement.append(el)
+  }
 })
 
 mainMenu.prepend(ulElement)
 
-console.log(`Aclin script is running into the website`)
+let divbody = document.createElement("div"),
+  body = document.getElementsByTagName("body")[0]
+divbody.classList.add("custom-background")
+body.prepend(divbody)
+
+console.log(`Aclin script ${version} is running into the website.`)
 // End of UL element
